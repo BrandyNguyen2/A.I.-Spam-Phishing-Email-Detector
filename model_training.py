00461a362042ed4546ffcc39d2ef450b58221b0a
bnguyen700 <brandy.nguyen02@gmail.com>
2024-12-08 20:43:39 -0800

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
@@ -40,3 +41,20 @@ print("Classification Report:")
 print(classification_report(y_test, y_pred))
 print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
+
+def test_custom_input(model, vectorizer):
+    while True:
+        user_input = input("Enter a message to classify (or type 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            print("Exiting.")
+            break
+        # Transform the user input using the fitted vectorizer
+        input_vectorized = vectorizer.transform([user_input])
+        # Predict using the trained model
+        prediction = model.predict(input_vectorized)
+        if prediction[0] == 1:
+            print("Prediciton: Not Spam")
+        print("Prediction: Spam")
+
+# Call the function to test custom input
+test_custom_input(model, vectorizer)
